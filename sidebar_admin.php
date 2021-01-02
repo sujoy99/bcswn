46e75abc9643771680a866745332147d63e471d9
sujoy99 <scg.sujoy@gmail.com>
2021-01-02 22:40:23 +0600

    commit(02-01-2021)

---
+++ sidebar_admin.php
@@ -0,0 +1,31 @@
+<!-- sidebar starts  -->
+
+<div class="sidebar">
+
+    <div class="logo d-flex justify-content-center align-items-center" >
+        <!-- <img src="./image/ITbl.png" alt="" height="90px" width="90px"> -->
+    </div>
+    <div class="sidebar_item" id="sidebar_item">
+        <h1 class="text-white p-3 logo text-center" style="font-size:2vw;">Dashboard</h1>
+        <!-- <li id="profile"><a href="./u_profile2.php" style="font-size:2vw;">
+                <i class="fas fa-user pr-3"></i>Profile</a>
+
+        </li> -->
+        <li id="event"><a href="#" style="font-size:2vw;"><i class="fas fa-book pr-2"></i> Event</a></li>
+        <!-- <li><a href="javascript:void(0)" id="pay" style="font-size:2vw;"><i class="fas fa-tags "></i> Payment  <span class="fas fa-caret-down"></span></a>
+        
+            <ul id="value" style="padding-left:0">
+                <li><a href="#" style="font-size:2vw;"> <i class="fas fa-hand-point-right"></i> Membership </a> </li>
+                <li id="subscription"><a href="./subscription.php" style="font-size:2vw;"> <i class="fas fa-hand-point-right"></i>Subscription</a> </li>
+                <li><a href="#" style="font-size:2vw;"><i class="fas fa-hand-point-right"></i> Event</a> </li>
+
+            </ul>
+        </li> -->
+
+        <li><a href="./logout.php" style="font-size:2vw;"><i class="fas fa-sign-out-alt pr-1"></i> Logout</a></li>
+    </div>
+</div>
+
+<!-- sidebar ends  -->
+
+
