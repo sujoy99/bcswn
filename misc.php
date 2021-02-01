f33186d7a61965392fcf4584aeb7073841e12459
sujoy99 <48888636+sujoy99@users.noreply.github.com>
2021-02-01 17:57:52 +0600

    Create misc.php

---
+++ misc.php
@@ -0,0 +1,268 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700;800;900&display=swap" rel="stylesheet">
+    <title>MiscBill</title>
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&family=Roboto:wght@300;400;500;700;900&display=swap"
+        rel="stylesheet">
+    <!--fontawesome-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="font/font/flaticon.css">
+
+   
+    <link rel="stylesheet" href="style2.css">
+    
+  
+
+    
+
+
+
+</head>
+
+
+
+
+
+<body>
+
+
+    <div id="wrapper">
+        <div class="overlay"></div>
+        
+<!--         sidebar starts  -->
+
+
+
+ <!-- Sidebar Start -->
+        <nav class="fixed-top align-top" id="sidebar-wrapper" role="navigation" style="background-color:#fff; border-right:3px solid #ff983f ;">
+            <div class="simplebar-content" style="padding: 0px;  ">
+                <!-- <div class="box text-center mt-4">
+
+            <img src="./img/Logo.png" alt="" height="80px" width="80px" >
+        </div> -->
+                <!-- <a class="sidebar-brand" href="index.html">  -->
+                <!-- </a> -->
+
+                <ul class="navbar-nav align-self-stretch"  >
+
+
+                    <li class="">
+
+                        <h3 class="text-center  mt-2" style="color:#ff983f">Dashboard1</h3>
+
+                    </li>
+
+                    <li class="">
+                        <a href="#" class="nav-link text-left" role="button">
+                        <img src="./image/electricity.png" alt="" height="10%" width="10%" ><b>  Energy Bill </b>
+
+                        </a>
+                    <li class="">
+                        <a href="#" class="nav-link text-left" role="button">
+                            <img src="./image/ledger.png" alt="" height="10%" width="10%" style="margin:2"><b>  Ledger</b>
+
+                        </a>
+                    </li>
+
+                    <li class="">
+                        <a href="#" class="nav-link text-left" role="button">
+                        <img src="./image/misc.png" alt="" height="10%" width="10%" style="margin:2"> <b>  Misc Bill</b>
+                        </a>
+                    </li>
+                    <li class="">
+                        <a href="#" class="nav-link text-left" role="button">
+                        <img src="./image/payment.png" alt="" height="10%" width="10%" style="margin:2"> <b >  Payment</b>
+                        </a>
+                    </li>
+                    <li class="">
+                        <a href="#" class="nav-link text-left" role="button">
+                        <img src="./image/misctariff.png" alt="" height="10%" width="10%" style="margin:2"><b>  Misc
+                                Tariff</b>
+                        </a>
+                    </li>
+                    <!-- <li class="">
+                        <a href="#" class="nav-link text-left" role="button">
+                       
+                        </a> -->
+                        <li>
+                        <a class="nav-link collapsed text-left" href="#" role="button" data-toggle="collapse"
+                            data-target="#collapseExample2">
+                            <img src="./image/report.png" alt="" height="10%" width="10%" style="margin:3"> <b>   Report </b>
+                             <i class="fas fa-arrow-down float-right "></i>
+
+                        </a>
+                        <div class="collapse menu mega-dropdown" id="collapseExample2">
+                            <div class="dropmenu" aria-labelledby="navbarDropdown">
+                                <div class="container-fluid " >
+                                    <div class="row">
+                                        <div class="col-lg-12 px-2">
+                                            <div class="submenu-box" >
+                                                <ul class="list-unstyled m-0">
+                                                    <li><a href="">Data Wise Summary</a></li>
+                                                    <li><a href="">Bill Type Wise Summary</a></li>
+                                                    <li><a href="">Tariff Type Wise Summary</a></li>
+                                                    <li><a href="">Tariff Wise Summary</a></li>
+                                                    <li><a href="">Business Type Wise Summary</a></li>
+                                                    <li><a href="">Usage Type Wise Summary</a></li>
+                                                    <li><a href="">Phase Type Wise Summary</a></li>
+                                                    <li><a href="">Feeder No Wise Summary</a></li>
+                                                    <li><a href="">Bill Group Wise Summary</a></li>
+                                                    <li><a href="">Book Wise Summary</a></li>
+                                                    <span class="fa-arrow"></span>
+                                                </ul>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                        
+                    </li>
+                    <li class="">
+                        <a href="#" class="nav-link text-left" role="button" >
+                        <i class="fas fa-power-off fa-5x pl-1" ></i><b>Logout</b>
+                        </a>
+                    </li>
+
+                </ul>
+
+
+            </div>
+
+
+
+            <!-- footer start  -->
+            <div class="brand_height" style="margin: 0 auto;">
+                
+                <center style="color: #ff983f;">Powered By</center>
+                <center style="color: #ff983f;"><b>
+                        <h4>IT Bangla Ltd. </h4>
+                    </b></center>
+            </div>
+
+
+            <!-- footer end  -->
+
+
+        </nav>
+
+        <!-- Sidebar End  -->
+
+</div>
+
+
+
+<!-- sidebar ends -->
+
+
+       
+
+        <!-- Page Content -->
+
+        <div id="page-content-wrapper" >
+
+            <div id="content">
+
+                <div class="container-fluid p-0 px-lg-0 px-md-0" style="background-color:#ff983f">
+                    <!-- Topbar -->
+                    <nav class="navbar navbar-expand navbar-light my-navbar">
+
+                        <!-- Sidebar Toggle (Topbar) -->
+                        <div type="button" id="bar" class="nav-icon1 hamburger animated fadeInLeft is-closed mt-2"
+                            data-toggle="offcanvas">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+
+
+                        <!-- Topbar Search -->
+                        
+                        <form class="  form-inline navbar-search pt-2"  style="color:#fff;">
+                            <h1 class="mx-5">MiscBill-<span style="font-size:25px"> Spot Miscellaneous Billing
+                                    System</span></h1>
+                </div>
+            </div>
+            </form>
+
+            <!-- page content starts  -->
+
+
+
+            <!-- page content ends  -->
+            <!-- End of Topbar -->
+
+
+            <!-- content start  -->
+
+          
+  
+
+   
+
+
+
+            <!-- content end  -->
+
+
+        </div>
+
+
+
+
+
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+            integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+        </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+        </script>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.js"
+            integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+
+
+       <script th:inline="javascript">
+        $(document).ready(function() {
+            $('#page-content-wrapper ,#sidebar-wrapper').toggleClass('toggled');
+        });
+
+
+
+        $('#bar').click(function() {
+            $(this).toggleClass('open');
+            $('#page-content-wrapper ,#sidebar-wrapper').toggleClass('toggled');
+
+        });
+        </script>
+
+
+
+
+
+
+</body>
+
+</html>
